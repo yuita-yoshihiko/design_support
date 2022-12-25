e2454c9103c23670ad05d3ab44fbfb0a9a36a9a7
yuita-yoshihiko <soup998211@gmail.com>
2022-12-25 11:33:29 +0900

    modify:トップページのルーティング変更 #11

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/top'
+  root 'home#top'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
