8be3c901beabfdacbc157f2b87e34c27e06456c7
yuita-yoshihiko <soup998211@gmail.com>
2023-01-22 22:32:39 +0900

    fix:トップページのJavaScriptの処理が発火しないバグを修正する #144

--- app/javascript/controllers/view_controller.js
+++ app/javascript/controllers/view_controller.js
@@ -1,22 +1,32 @@
-document.addEventListener("turbo:load", function () {
-  // タブに対してクリックイベントを適用
-  const tabs = document.getElementsByClassName('tab');
-  for(let i = 0; i < tabs.length; i++) {
-    tabs[i].addEventListener('click', tabSwitch, false);
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="navbar"
+export default class extends Controller {
+  static targets = ["menu", "content"]
+
+  /* メニュータブをクリックしたら発火するアクション */
+  menuClick(event){
+    const menus = this.menuTargets // メニューバー全体の要素
+    const current = event.currentTarget // クリックしたメニューバーの要素（現在地）
+    const currentIndex = menus.indexOf(current) // クリックしたメニューバーのインデックス番号
+    const contents = this.contentTargets // コンテンツ全体の要素
+
+    menus.forEach((menu, index)=>{ // 初期化
+      if(current.classList.contains("not-active")){
+        menu.classList.remove("is-active")
+        menu.classList.add("not-active")
+        contents[index].classList.add("hidden")
+      }
+    })
+
+    if(current.classList.contains("not-active")){ // クリックしたメニューのclassをアクティブにする
+      current.classList.remove("not-active")
+      current.classList.add("is-active")
+      contents[currentIndex].classList.remove("hidden") // クリックした要素のコンテンツを表示する
+    }
   }
 
-  // タブをクリックすると実行する関数
-  function tabSwitch(){
-    // タブのclassの値変更
-    document.getElementsByClassName('is-active')[0].classList.remove('is-active');
-    this.classList.add('is-active');
-    // コンテンツのclassの値変更
-    document.getElementsByClassName('is-show')[0].classList.remove('is-show');
-    const arrayTabs = Array.prototype.slice.call(tabs);
-    const index = arrayTabs.indexOf(this);
-    document.getElementsByClassName('panel')[index].classList.add('is-show');
-  };
-}, false);
+}
 
 $(function() {
   $('#nav li').on("click mouseover", function(e) {
