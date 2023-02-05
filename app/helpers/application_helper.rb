36992ba3c1c4a980b4a9acadbe8646945894713f
yuita-yoshihiko <soup998211@gmail.com>
2023-02-05 15:16:33 +0900

    add:ogp画像の設定とmetaタグのテキストの修正を行う #127

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,21 @@
 module ApplicationHelper
   def default_meta_tags
     {
-      site: '誰でもWebデザイン',
+      site: '誰でもデザイン',
       title: 'Web系デザイン初学者向けの情報検索サービス',
       reverse: true,
       separator: '|',
-      description: '「誰でもWebデザイン」は、Web系デザイン初学者向けの情報検索サービスです。学び方やジャンルごとに、自分に合った情報を見つけることができます。',
+      description: '「誰でもデザイン」は、Web系デザイン初学者向けの情報検索サービスです。学び方やジャンルごとに、自分に合った情報を見つけることができます。',
       charset: 'UTF-8',
       canonical: request.original_url,
       noindex: !Rails.env.production?,
       og: {
-        site_name: '誰でもWebデザイン',
+        site_name: '誰でもデザイン',
         title: 'Web系デザイン初学者向けの情報検索サービス',
-        description: '「誰でもWebデザイン」は、Web系デザイン初学者向けの情報検索サービスです。学び方やジャンルごとに、自分に合った情報を見つけることができます。',
+        description: '「誰でもデザイン」は、Web系デザイン初学者向けの情報検索サービスです。学び方やジャンルごとに、自分に合った情報を見つけることができます。',
         type: 'website',
         url: request.original_url,
+        image: image_url('ogp.jpg'),
         locale: 'ja_JP',
       },
     }
