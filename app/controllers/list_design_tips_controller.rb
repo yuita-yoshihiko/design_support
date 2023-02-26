5eaabbb65fdde56d31f8aec85edaf22df2ff9c92
yuita-yoshihiko <soup998211@gmail.com>
2023-02-26 21:03:38 +0900

    modify:リストへの情報登録時、バリデーションエラーが発生した場合エラーページへ遷移するよう実装する #248

--- app/controllers/list_design_tips_controller.rb
+++ app/controllers/list_design_tips_controller.rb
@@ -10,7 +10,13 @@ class ListDesignTipsController < ApplicationController
   def create
     @list = List.find(params[:list_id])
     @design_tip = DesignTip.find(params[:design_tip_id])
-    @list.design_tips << @design_tip
+    begin
+      @list.design_tips << @design_tip
+    rescue => e
+      flash.now[:error] = "その情報は既にリストに登録されています"
+      render :error, status: :unprocessable_entity
+      return
+    end
     if @list.save
       redirect_to list_design_tips_path, success: "リストに情報を追加しました。"
     else
