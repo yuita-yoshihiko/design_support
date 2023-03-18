fc1daf87c403b241713d9e7fe546e5349f50fec2
yuita-yoshihiko <soup998211@gmail.com>
2023-03-18 17:08:02 +0900

    fix:パスワードリセット機能の修正 #296

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,8 +71,8 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'smtp.gmail.com',
-    user_name: Rails.application.credentials.dig(:smtp, :gmail_address),
-    password: Rails.application.credentials.dig(:smtp, :gmail_password),
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_PASSWORD'],
     authentication: :login,
     enable_starttls_auto: true
   }
