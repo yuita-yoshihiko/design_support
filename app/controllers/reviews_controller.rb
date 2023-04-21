e3d3790aa5607a7aa2616bac23be5ebba69dd70e
yuita-yoshihiko <soup998211@gmail.com>
2023-04-21 23:43:43 +0900

    add:レビューを行うロジックの実装 #373

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,25 @@
 class ReviewsController < ApplicationController
+  def index
+    @design_tip_ids = DesignTip.pluck(:id)
+    @average_scores = {}
+    @design_tip_ids.each do |design_tip_id|
+      @average_scores[design_tip_id] = Review.average_score_by_design_tip_id(design_tip_id)
+    end
+  end
+
+  def create
+    @review = Review.find_or_initialize_by(user_id: current_user.id, design_tip_id: review_params[:design_tip_id])
+    @review.score = review_params[:score]
+    if @review.save
+      redirect_to design_tips_path(q: { tags_name_cont: 'テクニック' })
+    else
+      @design_tip = DesignTip.find(params[:id])
+      render 'design_tips/show'
+    end
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:design_tip_id, :score)
+  end
 end
