f3d7ca1e23fef457810490ae531d9d42318c9f8a
yuita-yoshihiko <soup998211@gmail.com>
2023-05-03 11:35:03 +0900

    add:回答に応じて正解不正解を表示する機能の実装 #412

--- app/controllers/quizees_controller.rb
+++ app/controllers/quizees_controller.rb
@@ -1,4 +1,21 @@
 class QuizeesController < ApplicationController
+  skip_before_action :require_login
+
   def index
+    @quizees = Quiz.all
+    @choices = Choice.all
+    check_answer
+  end
+
+  def commentary
+  end
+
+  def check_answer
+    correct = params[:correct]
+    if correct == "true"
+      @result = "正解！"
+    elsif correct == "false"
+      @result = "不正解..."
+    end
   end
 end
