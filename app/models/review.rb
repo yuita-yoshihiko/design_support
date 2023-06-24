8048343ec739a3dd2350db9496aa3de718c962a3
yuita-yoshihiko <soup998211@gmail.com>
2023-06-24 18:36:57 +0900

    modify:reviewモデルのテスト修正 #505

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,7 @@ class Review < ApplicationRecord
   belongs_to :design_tip
 
   validates :user_id, uniqueness: { scope: :design_tip_id }
+  validates :user_id, presence: true
+  validates :design_tip_id, presence: true
   validates :score, presence: true
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:review) { create(:review) }
+
+  it "user_id、design_tip_id、評価があれば登録できること" do
+    expect(review).to be_valid
+  end
+
+  it "user_idがなければ登録できないこと" do
+    review.user_id = nil
+    review.valid?
+    expect(review.errors[:user_id]).to include("を入力してください")
+  end
+
+  it "design_tip_idがなければ登録できないこと" do
+    review.design_tip_id = nil
+    review.valid?
+    expect(review.errors[:design_tip_id]).to include("を入力してください")
+  end
+
+  it "評価がなければ登録できないこと" do
+    review.score = nil
+    review.valid?
+    expect(review.errors[:score]).to include("を入力してください")
+  end
 end
