2aaeccac8f2134817dca6043e584e8efd5a7d448
yuita-yoshihiko <soup998211@gmail.com>
2023-06-24 20:10:33 +0900

    add:choiceモデルのテスト追加 507

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,6 +1,6 @@
 class Choice < ApplicationRecord
   belongs_to :quiz
 
-  validates :content, presence: true, length: { maximum: 65_535 }
+  validates :content, presence: true, length: { maximum: 500 }
   validates :correct, presence: true
 end
